546c16f631468fc5e6e8109049a12ea0eea608d6
Demetrios Pantaleão <DemetriosP>
2023-03-21 21:44:19 -0300

    Implementação de regra de promoção

---
+++ src/br/com/alura/rh/promocao/PromocaoService.java
@@ -0,0 +1,26 @@
+package br.com.alura.rh.promocao;
+
+import br.com.alura.rh.ValidacaoException;
+import br.com.alura.rh.model.Cargo;
+import br.com.alura.rh.model.Funcionario;
+
+public class PromocaoService {
+
+    public void promover(Funcionario funcionario, boolean metaBatida){
+
+        Cargo cargoAtual = funcionario.getCargo();
+
+        if(Cargo.GERENTE == cargoAtual) {
+            throw new ValidacaoException("Gerentes não podem ser promovidos");
+        }
+
+        if(metaBatida){
+            Cargo novCargo = cargoAtual.getProximoCargo();
+            funcionario.promover(novCargo);
+        }else{
+            throw new ValidacaoException("Funcionário não bateu a meta");
+        }
+
+    }
+    
+}
