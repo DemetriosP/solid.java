5ed5bcb637b582e13cb088ea4e0bd381840a85d7
Demetrios Pantaleão <DemetriosP>
2023-03-21 22:02:58 -0300

    Aplicando composição na classe Funcionário

--- src/br/com/alura/rh/service/ReajusteService.java
+++ src/br/com/alura/rh/service/ReajusteService.java
@@ -1,10 +1,7 @@
 package br.com.alura.rh.service;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.List;
-
-import br.com.alura.rh.ValidacaoException;
 import br.com.alura.rh.model.Funcionario;
 
 public class ReajusteService {
@@ -17,7 +14,7 @@ public class ReajusteService {
 
     public void reajustarSalario(Funcionario funcionario, BigDecimal aumento) {
 		this.validacoes.forEach(v -> v.validar(funcionario, aumento));
-        BigDecimal salarioReajustado = funcionario.getSalario().add(aumento);
+        BigDecimal salarioReajustado = funcionario.getDadosPessoais().getSalario().add(aumento);
         funcionario.atualizaSalario(salarioReajustado);
 	}
     
